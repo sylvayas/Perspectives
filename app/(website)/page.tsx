683064b2ebf4c4d233554761db173fb10d12f49a
AITECHCI <aitechci1@gmail.com>
2025-01-20 15:57:43 +0000

    Commit 1

---
+++ app/(website)/page.tsx
@@ -0,0 +1,19 @@
+import Hero from "@/components/website/Hero";
+import Solutions from "@/components/website/Solutions/solutions";
+import ShowcasePartenaires from "@/components/website/Partenaires/showcasepartenaires";
+import Showcase from "@/components/website/Showcase/showcase";
+import About from "@/components/website/About/about";
+import Statistiques from "@/components/website/Statistiques/statistiques";
+
+export default async function Home() {
+  return (
+    <div>
+      <Hero />
+      <Solutions />
+      <Showcase />
+      <ShowcasePartenaires />
+      <About />
+      <Statistiques />
+    </div>
+  );
+}
