288d1b50e9cce3ebae24f3946ace340f21cd7fc8
AITECHCI <aitechci1@gmail.com>
2025-01-29 10:16:28 +0000

    Services 1

--- components/website/Services/services.tsx
+++ components/website/Services/services.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
-import SolutionsCarousel from "./solutions-carousel"
+import ServicesCarousel from "./services-carousel"
 
-export default function Solutions() {
+export default function Services() {
     return (
         <section id="showcase" className="container px-2 md:px-8 py-14">
             <h2 className="text-novis_yellow mb-2 text-center font-medium text-2xl md:text-4xl tracking-tight font-saudagar">
@@ -11,7 +11,7 @@ export default function Solutions() {
             Nous proposons des services variés en finance, immobilier, nettoyage, commerce général et transport, incluant conseil, accompagnement, gestion de biens, location, import-export, et solutions logistiques.
             </h3>
 
-            <SolutionsCarousel />
+            <ServicesCarousel />
         </section>
 
     )
